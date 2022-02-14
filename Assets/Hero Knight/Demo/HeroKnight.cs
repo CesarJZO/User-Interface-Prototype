b6fb3b2b284c90dbfc17a940cb7a11481a0fea48
CesarJZO <cesarjzo@outlook.com>
2022-02-13 21:40:14 -0600

    Implement Run animation

--- Assets/Hero Knight/Demo/HeroKnight.cs
+++ Assets/Hero Knight/Demo/HeroKnight.cs
@@ -4,16 +4,17 @@ using UnityEngine.Events;
 
 public class HeroKnight : MonoBehaviour
 {
+    public static Vector3 Velocity;
     [SerializeField] float      m_speed = 4.0f;
     [SerializeField] float      m_jumpForce = 7.5f;
     [SerializeField] float      m_rollForce = 6.0f;
     [SerializeField] bool       m_noBlood = false;
     [SerializeField] GameObject m_slideDust;
 
-    [SerializeField] float _effectsDuration;
-    public UnityEvent<float> OnPlayerHit;
-    public UnityEvent OnPlayerRun;
-    public UnityEvent<Vector3> OnPlayerMove;
+    [SerializeField] float      _effectsDuration;
+    public UnityEvent<float>    OnPlayerHit;
+    public UnityEvent<bool>     OnPlayerRun;
+    public UnityEvent<Vector3>  OnPlayerMove;
 
     private Animator            m_animator;
     private Rigidbody2D         m_body2d;
@@ -69,6 +70,7 @@ public class HeroKnight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Velocity = _rigidbody.velocity;
         // Increase timer that controls attack combo
         m_timeSinceAttack += Time.deltaTime;
 
@@ -188,7 +190,7 @@ public class HeroKnight : MonoBehaviour
             // Reset timer
             m_delayToIdle = 0.05f;
             m_animator.SetInteger("AnimState", 1);
-            OnPlayerRun?.Invoke();
+            OnPlayerRun?.Invoke(true && m_grounded);
         }
 
         //Idle
@@ -198,6 +200,7 @@ public class HeroKnight : MonoBehaviour
             m_delayToIdle -= Time.deltaTime;
             if (m_delayToIdle < 0)
                 m_animator.SetInteger("AnimState", 0);
+            OnPlayerRun?.Invoke(false);
         }
     }
 
--- Assets/Scripts/HUD/Runner.cs
+++ Assets/Scripts/HUD/Runner.cs
@@ -3,21 +3,33 @@ namespace HUDAnimations
 {
     public class Runner : MonoBehaviour
     {
+        [SerializeField] RectTransform _centerReference;
+        [SerializeField] Mover _moverScript;
         [SerializeField] float _speed = 5;
         [SerializeField] float _magnitude = 2;
         float _elapsed = 0;
-        Vector3 _mover;
+        Vector3 _direction;
         RectTransform _rectTransform;
         void Awake()
         {
-            _mover = new Vector2();
+            _direction = new Vector2();
             _rectTransform = GetComponent<RectTransform>();
         }
-        public void Run()
+        public void Run(bool isRunning)
         {
-            _mover.y += Mathf.Sin(_elapsed * _speed) * _magnitude;
-            _rectTransform.position += _mover;
-            _elapsed += 0.01f;
+            if (isRunning)
+            {
+                _direction.x = _rectTransform.position.x;
+                _direction.y = _rectTransform.position.y + Mathf.Sin(Time.time * _speed) * _magnitude;
+                _rectTransform.position = _direction;
+                _elapsed += 0.01f;
+            }
+            else
+            {
+                Vector3 zero = Vector3.zero;
+                _rectTransform.position = Vector3.SmoothDamp(
+                    _rectTransform.position, _centerReference.position, ref zero, 0.1f);
+            }
         }
     }
 }
