52edb8a8c066901dc17d76cca43b5e8afa81e668
CesarJZO <cesarjzo@outlook.com>
2022-02-13 22:04:42 -0600

    Create Runner GameObject

--- Assets/Scripts/HUD/Runner.cs
+++ Assets/Scripts/HUD/Runner.cs
@@ -4,10 +4,8 @@ namespace HUDAnimations
     public class Runner : MonoBehaviour
     {
         [SerializeField] RectTransform _centerReference;
-        [SerializeField] Mover _moverScript;
         [SerializeField] float _speed = 5;
         [SerializeField] float _magnitude = 2;
-        float _elapsed = 0;
         Vector3 _direction;
         RectTransform _rectTransform;
         void Awake()
@@ -22,7 +20,6 @@ namespace HUDAnimations
                 _direction.x = _rectTransform.position.x;
                 _direction.y = _rectTransform.position.y + Mathf.Sin(Time.time * _speed) * _magnitude;
                 _rectTransform.position = _direction;
-                _elapsed += 0.01f;
             }
             else
             {
